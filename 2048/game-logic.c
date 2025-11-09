c174dd062abaac831b8f325548c324d2422cf8e2
Austin Thein <amthein321@gmail.com>
2025-11-09 02:16:21 -0500

    Finish game logic potentially

--- 2048/game-logic.c
+++ 2048/game-logic.c
@@ -1,91 +1,118 @@
-#include <stdlib.h>
 #include <stdint.h>
-
-
+#include <stdlib.h>
 
 typedef struct {
-    uint8_t game_board[4][4]; // 4x4 Matrix of powers of 2 corresponing to the value in that position as 2^(i,j) 
+    uint8_t game_board[4][4]; // 4x4 Matrix of powers of 2 corresponing to the
+                              // value in that position as 2^(i,j)
 } GameState;
 
-// Initialize game board. This places two blocks on the board, one a 2-block and the other a 4-block or a 2-block 
-void game_init(GameState * gs){}
+// Initialize game board. This places two blocks on the board, one a 2-block and
+// the other a 4-block or a 2-block
+void game_init(GameState *gs) {
+}
 
-// Cascade blocks up, combining equal powers 
-void game_move_up(GameState * gs){
+#define move_vertical(from, onto)                                              \
+    for (i = 0; i < 4; i++) {                                                  \
+        uint8_t *onto_tile = &gs->game_board[onto][i];                         \
+        uint8_t *from_tile = &gs->game_board[from][i];                         \
+        if (!(smush_flag & (1 << ((onto * 4) + i)))) {                         \
+            if (*onto_tile == 0) {                                             \
+                /*If there is empty space on the space being moved onto, fill  \
+                 * that spot with the tile that it came from and clear the     \
+                 * from tile */                                                \
+                *onto_tile = *from_tile;                                       \
+                *from_tile = 0;                                                \
+            } else if (*onto_tile == *from_tile) {                             \
+                /*If the tiles are the same, add one to the onto and clear     \
+                 * from */                                                     \
+                *onto_tile += 1;                                               \
+                *from_tile = 0;                                                \
+                smush_flag |= 1 << ((onto * 4) + i);                           \
+            }                                                                  \
+        }                                                                      \
+    }
 
+#define move_horizontal(from, onto)                                            \
+    for (i = 0; i < 4; i++) {                                                  \
+        uint8_t *onto_tile = &gs->game_board[i][onto];                         \
+        uint8_t *from_tile = &gs->game_board[i][from];                         \
+        if (!(smush_flag & (1 << ((i * 4) + onto)))) {                         \
+            if (*onto_tile == 0) {                                             \
+                /*If there is empty space on the space being moved onto, fill  \
+                 * that spot with the tile that it came from and clear the     \
+                 * from tile */                                                \
+                *onto_tile = *from_tile;                                       \
+                *from_tile = 0;                                                \
+            } else if (*onto_tile == *from_tile) {                             \
+                /*If the tiles are the same, add one to the onto and clear     \
+                 * from */                                                     \
+                *onto_tile += 1;                                               \
+                *from_tile = 0;                                                \
+                smush_flag |= 1 << ((i * 4) + onto);                           \
+            }                                                                  \
+        }                                                                      \
+    }
 
-    // 16 bit "Matrix" of bit flags signaling whether the corresponding cell has been combined
-    uint16_t smush_flag;    // Each flag bit (row*4)+col corresponds to 
-    int j, level;
-
-    for (level = 1; level < 3; level++){                                // Increments level such that the rows are traversed
+// Cascade blocks up, combining equal powers
+void game_move_up(GameState *gs) {
+    uint16_t smush_flag;
+    int i, level;
+    for (level = 0; level < 3; level++) {
         switch (level) {
-        case 3:
-            for (j = 0; j < 3; j++) {                   
-                uint8_t *up_cell_value = &gs->game_board[2][j];
-                uint8_t *cell_value = &gs->game_board[3][j];
-                if (!(smush_flag & (1 << ((3*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
-                    if (*up_cell_value == 0){                   // If above cell is zero
-                        *up_cell_value = *cell_value;           // Copy cell to above cell   
-                        *cell_value = 0;                        // Zero out cell
-                    }else if (*up_cell_value == *cell_value){   // If cells match
-                        (*up_cell_value)++;                     // Increment above cell
-                        *cell_value = 0;                        // Zero cell
-                    } 
-                    smush_flag = smush_flag | (1 << ((2*4)+j)); // Mark above cell as modified
-                }
-            }
         case 2:
-            for (j = 0; j < 3; j++) {
-                uint8_t *up_cell_value = &gs->game_board[1][j];
-                uint8_t *cell_value = &gs->game_board[2][j];
-                if (!(smush_flag & (1 << ((2*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
-                    if (*up_cell_value == 0){                   // If above cell is zero
-                        *up_cell_value = *cell_value;           // Copy cell to above cell   
-                        *cell_value = 0;                        // Zero out cell
-                    }else if (*up_cell_value == *cell_value){   // If cells match
-                        (*up_cell_value)++;                     // Increment above cell
-                        *cell_value = 0;                        // Zero cell
-                    } 
-                    smush_flag = smush_flag | (1 << ((1*4)+j)); // Mark above cell as modified
-                }
-            }
+            move_vertical(3, 2);
         case 1:
-            for (j = 0; j < 3; j++) {
-                uint8_t *up_cell_value = &gs->game_board[0][j];
-                uint8_t *cell_value = &gs->game_board[1][j];
-                if (!(smush_flag & (1 << ((1*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
-                    if (*up_cell_value == 0){                   // If above cell is zero
-                        *up_cell_value = *cell_value;           // Copy cell to above cell   
-                        *cell_value = 0;                        // Zero out cell
-                    }else if (*up_cell_value == *cell_value){   // If cells match
-                        (*up_cell_value)++;                     // Increment above cell
-                        *cell_value = 0;                        // Zero cell
-                    } 
-                    smush_flag = smush_flag | (1 << ((0*4)+j)); // Mark above cell as modified
-                }
-            }
+            move_vertical(2, 1);
+        case 0:
+            move_vertical(1, 0);
         }
     }
 }
 
-// Cascade blocks down, combining equal powers 
-void game_move_down(GameState *){
-
-
-
+// Cascade blocks down, combining equal powers
+void game_move_down(GameState *gs) {
+    uint16_t smush_flag;
+    int i, level;
+    for (level = 0; level < 3; level++) {
+        switch (level) {
+        case 2:
+            move_vertical(0, 1);
+        case 1:
+            move_vertical(1, 2);
+        case 0:
+            move_vertical(2, 3);
+        }
+    }
 }
 
-// Cascade blocks left, combining equal powers 
-void game_move_left(GameState *){
-
-
-
+// Cascade blocks left, combining equal powers
+void game_move_left(GameState *gs) {
+    uint16_t smush_flag;
+    int i, level;
+    for (level = 0; level < 3; level++) {
+        switch (level) {
+        case 2:
+            move_horizontal(0, 1);
+        case 1:
+            move_horizontal(1, 2);
+        case 0:
+            move_horizontal(2, 3);
+        }
+    }
 }
 
-// Cascade blocks right, combining equal powers 
-void game_move_right(GameState *){
-
-
-
+// Cascade blocks right, combining equal powers
+void game_move_right(GameState *gs) {
+    uint16_t smush_flag;
+    int i, level;
+    for (level = 0; level < 3; level++) {
+        switch (level) {
+        case 2:
+            move_horizontal(3, 2);
+        case 1:
+            move_horizontal(2, 1);
+        case 0:
+            move_horizontal(1, 0);
+        }
+    }
 }
