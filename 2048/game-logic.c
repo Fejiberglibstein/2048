6264ee1bf86d123449d2b7470902ba34c0dedb0e
Austin Thein <amthein321@gmail.com>
2025-11-09 01:50:17 -0500

    fix some game logic stuff

--- 2048/game-logic.c
+++ 2048/game-logic.c
@@ -4,7 +4,7 @@
 
 
 typedef struct {
-    uint8_t game_board[4][4] ;          // 4x4 Matrix of powers of 2 corresponing to the value in that position as 2^(i,j) 
+    uint8_t game_board[4][4]; // 4x4 Matrix of powers of 2 corresponing to the value in that position as 2^(i,j) 
 } GameState;
 
 // Initialize game board. This places two blocks on the board, one a 2-block and the other a 4-block or a 2-block 
@@ -16,61 +16,57 @@ void game_move_up(GameState * gs){
 
     // 16 bit "Matrix" of bit flags signaling whether the corresponding cell has been combined
     uint16_t smush_flag;    // Each flag bit (row*4)+col corresponds to 
-    int i, j, level;
+    int j, level;
 
-    for (level = 1; level < 3; i++){                                // Increments level such that the rows are traversed
-                                                                    // 1, 2-1, 3-2-1, end
+    for (level = 1; level < 3; level++){                                // Increments level such that the rows are traversed
         switch (level) {
-            case 3:
-                for (j = 0; j < 3; j++) {                   
-                    uint8_t *up_cell_value = &gs->game_board[2][j];
-                    uint8_t *cell_value = &gs->game_board[3][j];
-                    if (!(smush_flag & (1 << ((3*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
-                        if (*up_cell_value == 0){                   // If above cell is zero
-                            *up_cell_value = *cell_value;           // Copy cell to above cell   
-                            *cell_value = 0;                        // Zero out cell
-                        }else if (*up_cell_value == *cell_value){   // If cells match
-                            *up_cell_value++;                       // Increment above cell
-                            *cell_value = 0;                        // Zero cell
-                        } 
-                        smush_flag = smush_flag | (1 << ((2*4)+j)); // Mark above cell as modified
-                    }
+        case 3:
+            for (j = 0; j < 3; j++) {                   
+                uint8_t *up_cell_value = &gs->game_board[2][j];
+                uint8_t *cell_value = &gs->game_board[3][j];
+                if (!(smush_flag & (1 << ((3*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
+                    if (*up_cell_value == 0){                   // If above cell is zero
+                        *up_cell_value = *cell_value;           // Copy cell to above cell   
+                        *cell_value = 0;                        // Zero out cell
+                    }else if (*up_cell_value == *cell_value){   // If cells match
+                        (*up_cell_value)++;                     // Increment above cell
+                        *cell_value = 0;                        // Zero cell
+                    } 
+                    smush_flag = smush_flag | (1 << ((2*4)+j)); // Mark above cell as modified
                 }
-            case 2:
-                for (j = 0; j < 3; j++) {
-                    uint8_t *up_cell_value = &gs->game_board[1][j];
-                    uint8_t *cell_value = &gs->game_board[2][j];
-                    if (!(smush_flag & (1 << ((2*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
-                        if (*up_cell_value == 0){                   // If above cell is zero
-                            *up_cell_value = *cell_value;           // Copy cell to above cell   
-                            *cell_value = 0;                        // Zero out cell
-                        }else if (*up_cell_value == *cell_value){   // If cells match
-                            *up_cell_value++;                       // Increment above cell
-                            *cell_value = 0;                        // Zero cell
-                        } 
-                        smush_flag = smush_flag | (1 << ((1*4)+j)); // Mark above cell as modified
-                    }
+            }
+        case 2:
+            for (j = 0; j < 3; j++) {
+                uint8_t *up_cell_value = &gs->game_board[1][j];
+                uint8_t *cell_value = &gs->game_board[2][j];
+                if (!(smush_flag & (1 << ((2*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
+                    if (*up_cell_value == 0){                   // If above cell is zero
+                        *up_cell_value = *cell_value;           // Copy cell to above cell   
+                        *cell_value = 0;                        // Zero out cell
+                    }else if (*up_cell_value == *cell_value){   // If cells match
+                        (*up_cell_value)++;                     // Increment above cell
+                        *cell_value = 0;                        // Zero cell
+                    } 
+                    smush_flag = smush_flag | (1 << ((1*4)+j)); // Mark above cell as modified
                 }
-            case 1:
-                for (j = 0; j < 3; j++) {
-                    uint8_t *up_cell_value = &gs->game_board[0][j];
-                    uint8_t *cell_value = &gs->game_board[1][j];
-                    if (!(smush_flag & (1 << ((1*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
-                        if (*up_cell_value == 0){                   // If above cell is zero
-                            *up_cell_value = *cell_value;           // Copy cell to above cell   
-                            *cell_value = 0;                        // Zero out cell
-                        }else if (*up_cell_value == *cell_value){   // If cells match
-                            *up_cell_value++;                       // Increment above cell
-                            *cell_value = 0;                        // Zero cell
-                        } 
-                        smush_flag = smush_flag | (1 << ((0*4)+j)); // Mark above cell as modified
-                    }
+            }
+        case 1:
+            for (j = 0; j < 3; j++) {
+                uint8_t *up_cell_value = &gs->game_board[0][j];
+                uint8_t *cell_value = &gs->game_board[1][j];
+                if (!(smush_flag & (1 << ((1*4)+j)))){          // Mask out corresponding smush bit and continue only if 0
+                    if (*up_cell_value == 0){                   // If above cell is zero
+                        *up_cell_value = *cell_value;           // Copy cell to above cell   
+                        *cell_value = 0;                        // Zero out cell
+                    }else if (*up_cell_value == *cell_value){   // If cells match
+                        (*up_cell_value)++;                     // Increment above cell
+                        *cell_value = 0;                        // Zero cell
+                    } 
+                    smush_flag = smush_flag | (1 << ((0*4)+j)); // Mark above cell as modified
                 }
+            }
         }
     }
-
-
-
 }
 
 // Cascade blocks down, combining equal powers 
--- main.c
+++ main.c
@@ -1,36 +1,51 @@
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <assert.h>
 #include "2048/include/game-logic.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
-int main(){
+bool check_board(GameState *exp, GameState *act);
 
-    struct GameState game;
-    game.game_board = {
-                        {2, 0, 0, 4}, 
-                        {2, 2, 0, 4}, 
-                        {0, 2, 2, 4}, 
-                        {0, 0, 2, 4}};
-    uint8_t expected[4][4]  = {
-                                {4, 4, 4, 8}, 
-                                {0, 0, 0, 8}, 
-                                {0, 0, 0, 0}, 
-                                {0, 0, 0, 0}};
-    struct GameState exp;
-    exp.game_board = expected;
+int main() {
+
+    GameState game = (GameState) {
+        .game_board = {
+            {2, 0, 0, 4},
+            {2, 2, 0, 4},
+            {0, 2, 2, 4},
+            {0, 0, 2, 4},
+        },
+    };
+    GameState expected = (GameState) {
+        .game_board = {
+            {4, 4, 4, 8},
+            {0, 0, 0, 8},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+        },
+    };
 
     game_move_up(&game);
-    assert(check_board(game, exp));
-
-
+    assert(check_board(&game, &expected));
 }
 
-bool check_board(struct GameState * exp, struct GameState * act){
+bool check_board(GameState *act, GameState *exp) {
     int i;
-    for (i = 0; i < 4; i++;){
-        if (!((uint32_t *)exp.game_board[i]) == ((uint32_t *)act.game_board[i])){
-            return false;   
+    int x, y;
+
+    for (y = 0; y < 4; y++) {
+        for (x = 0; x < 4; x++) {
+            printf("%d ", act->game_board[y][x]);
+        }
+        printf("\n");
+    }
+
+
+    for (i = 0; i < 4; i++) {
+        if (*((uint32_t *)exp->game_board[i]) != *((uint32_t *)act->game_board[i])) {
+            return false;
         }
     }
+
+    return true;
 }
