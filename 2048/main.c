d23fef58b671295b6f4f10d9ff179042e42dcc41
Austin Thein <amthein321@gmail.com>
2025-11-11 11:49:38 -0500

    Move clock init into main

--- 2048/main.c
+++ 2048/main.c
@@ -1,6 +1,31 @@
+#include "include/accelerometer.h"
+#include "include/gameplay.h"
 #include "include/matrix.h"
+#include "include/render.h"
 #include "include/tiva.h"
+#include <stdbool.h>
+
+char printf_arena[256];
+
+void clock_init() {
+    // Clear usesys and set byprass
+    *SYSCTL_RCC = (*SYSCTL_RCC & ~(1 << 22)) | (1 << 11);
+
+    uint32_t rcc = *SYSCTL_RCC;
+    rcc = (rcc & ~(0b11 << 4)) | 0b01 << 4; // select oscillator source (OSCSRC)
+    rcc = (rcc & ~(0b11111 << 6)) | 0x15 << 6; // set crystal value to 16MHz
+    rcc = (rcc & ~(0b1 << 13));                // Clear pwrdwn bit
+    rcc = (rcc & ~(0b1111 << 23)) | 0x2 << 23; // Set sysdiv to be 2
+    rcc |= 1 << 22;                            // Set the usesysdiv bit
+    *SYSCTL_RCC = rcc;
+
+    while (!(*SYSCTL_PLLSTAT)) // Wait until the status is 1
+        ;
+
+    *SYSCTL_RCC &= ~(1 << 11);
+}
 
 int main(void) {
-    matrix_init();
+    clock_init();
+    // matrix_init();
 }
--- 2048/matrix.c
+++ 2048/matrix.c
@@ -33,26 +33,8 @@ struct {
     uint8_t current_buf;
 } MatrixState __attribute__((aligned(32))) = {};
 
-void clock_init() {
-    // Clear usesys and set byprass
-    *SYSCTL_RCC = (*SYSCTL_RCC & ~(1 << 22)) | (1 << 11);
-
-    uint32_t rcc = *SYSCTL_RCC;
-    rcc = (rcc & ~(0b11 << 4)) | 0b01 << 4; // select oscillator source (OSCSRC)
-    rcc = (rcc & ~(0b11111 << 6)) | 0x15 << 6; // set crystal value to 16MHz
-    rcc = (rcc & ~(0b1 << 13));                // Clear pwrdwn bit
-    rcc = (rcc & ~(0b1111 << 23)) | 0x2 << 23; // Set sysdiv to be 2
-    rcc |= 1 << 22;                            // Set the usesysdiv bit
-    *SYSCTL_RCC = rcc;
-
-    while (!(*SYSCTL_PLLSTAT)) // Wait until the status is 1
-        ;
-
-    *SYSCTL_RCC &= ~(1 << 11);
-}
 
 void matrix_init() {
-    clock_init();
     //                                                                        //
     //                        Initialize GPIO                                 //
     //                                                                        //
