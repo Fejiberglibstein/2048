9855c3aa533dc3dc5eb964e65b21b680ec4567ad
Austin Thein <amthein321@gmail.com>
2025-11-11 09:43:30 -0500

    Get accelerometer working

--- 2048/accelerometer.c
+++ 2048/accelerometer.c
@@ -26,13 +26,12 @@ void accel_init() {
     *I2C_MCR(i2c_1) = 0x00000010; // Enable i2c as master
     // (16MHz/(2*(6+4)*100000))-1;
     *I2C_MTPR(i2c_1) = (66666666 / (2 * (6 + 4) * 100000)) - 1;
-
 }
 
 uint8_t accel_read(uint8_t reg_addr) {
     *I2C_MSA(i2c_1) = 0x32;            // Write address with transmit
-    *I2C_MDR(i2c_1) = reg_addr;        // Write data
-    *I2C_MCS(i2c_1) = 0b00111;         // Set run & start
+    *I2C_MDR(i2c_1) = reg_addr;        // Write address
+    *I2C_MCS(i2c_1) = 0b00111;         // Set run start, & stop
     while (*I2C_MCS(i2c_1) & (1 << 0)) // Loop while busy bit is high
         ;
     assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
@@ -48,16 +47,96 @@ uint8_t accel_read(uint8_t reg_addr) {
 
 void accel_write(uint8_t reg_addr, uint8_t data) {
     *I2C_MSA(i2c_1) = 0x32;            // Write address with transmit
-    *I2C_MDR(i2c_1) = reg_addr;        // Write data
-    *I2C_MCS(i2c_1) = 0b00111;         // Set run & start
+    *I2C_MDR(i2c_1) = reg_addr;        // Write address
+    *I2C_MCS(i2c_1) = 0b00011;         // Set run & start
     while (*I2C_MCS(i2c_1) & (1 << 0)) // Loop while busy bit is high
         ;
     assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
 
-    *I2C_MSA(i2c_1) = 0x32;            // Write address with receive
     *I2C_MDR(i2c_1) = data;            // Write data
-    *I2C_MCS(i2c_1) = 0b00111;         // Set run, start, and stop
+    *I2C_MCS(i2c_1) = 0b00101;         // Set run & stop
     while (*I2C_MCS(i2c_1) & (1 << 0)) // Loop while busy bit is high
         ;
     assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
+}
+
+void accel_get_acceleration(struct AccelerometerData *data) {
+    *I2C_MSA(i2c_1) = 0x32;                   // Write address with transmit
+    *I2C_MDR(i2c_1) = ACCEL_OUT_X_L_A | 0x80; // address with increment
+    *I2C_MCS(i2c_1) = 0b00011;                // Set run & start
+    while (*I2C_MCS(i2c_1) & (1 << 0))        // Loop while busy bit is high
+        ;
+    assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
+
+    // This is a 6 byte value
+    uint8_t *casted = (void *)data;
+
+    // Read the first byte
+    *I2C_MSA(i2c_1) = 0x33;            // Write address with receive
+    *I2C_MCS(i2c_1) = 0b01011;         // Set run, start, & ack
+    while (*I2C_MCS(i2c_1) & (1 << 0)) // Loop while busy bit is high
+        ;
+    assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
+
+    *casted++ = *I2C_MDR(i2c_1);
+
+    // Read the next 4 bytes
+    int i = 0;
+    for (i = 0; i < 4; i++) {
+        *I2C_MCS(i2c_1) = 0b01001;         // Set run & ack
+        while (*I2C_MCS(i2c_1) & (1 << 0)) // Loop while busy bit is high
+            ;
+        assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
+        *casted++ = *I2C_MDR(i2c_1);
+    }
+
+    // Read the last byte
+    *I2C_MCS(i2c_1) = 0b00101;         // Set run & stop
+    while (*I2C_MCS(i2c_1) & (1 << 0)) // Loop while busy bit is high
+        ;
+    assert((*I2C_MCS(i2c_1) & 0x0E) == 0); // assert no error
+    *casted++ = *I2C_MDR(i2c_1);
+}
+
+// Configures the accelerometer to cause xyz interrupts on PA2
+void accel_interrupts_init() {
+    uint8_t gpio_ports = 0b00000001; // interrupts is on port a
+    *SYSCTL_RCGCGPIO |= gpio_ports;
+    while ((*SYSCTL_PRGPIO & gpio_ports) != gpio_ports)
+        ;
+
+    uint8_t interrupt_pin = (1 << 2);           // Interrupt is on pin 2
+    *GPIO_DEN(gpio_port_a) |= interrupt_pin;    // turn it on
+    *GPIO_DIR(gpio_port_a) &= ~interrupt_pin;   // Toggle as input
+    *GPIO_AFSEL(gpio_port_a) &= ~interrupt_pin; // no afsel
+
+    // 1. Mask the corresponding port by clearing the IME field in the GPIOIM
+    // register.
+    *GPIO_IM(gpio_port_a) &= ~interrupt_pin; // disable interrupts
+    // 2. Configure the IS field in the GPIOIS register and the IBE field in the
+    // GPIOIBE register.
+    *GPIO_IBE(gpio_port_a) &= ~interrupt_pin; // one edge causes interrupt
+    *GPIO_IS(gpio_port_a) &= ~interrupt_pin;  // edge sensitive
+    *GPIO_IEV(gpio_port_a) |= interrupt_pin;  // Rising edge triggered
+    // 3. Clear the GPIORIS register.
+    *GPIO_RIS(gpio_port_a) = 0;
+    // 4. Unmask the port by setting the IME field in the GPIOIM register.
+    *GPIO_IM(gpio_port_a) |= interrupt_pin;
+    *REG(0xE000E100) |= 1 << 0; // GPIO Port a is 0th offset in vectable
+
+    accel_write(ACCEL_CTRL_REG5_A, 0x80); // Reset memory
+    accel_write(ACCEL_CTRL_REG1_A, 0x57); // ODR = 5, enable xyz
+    accel_write(ACCEL_CTRL_REG3_A, 0x60); // Enable interrupts for AOI1, AOI2
+    accel_write(ACCEL_CTRL_REG4_A, 0x80); // Block data update for CTRL_REG4
+    accel_write(ACCEL_CTRL_REG5_A, 0x40); // Latch interrupt on Int1
+    accel_write(ACCEL_CTRL_REG6_A, 0x02); // interrupts are active low
+    accel_write(ACCEL_INT1_CFG_A, 0x3F);  // all interrupts
+    assert(accel_read(ACCEL_INT1_CFG_A) == 0x3F); // make sure it worked
+    accel_write(ACCEL_INT1_THS_A, 100);
+    accel_write(ACCEL_INT1_DURATION_A, 0x01); // Duration 1
+}
+
+void accelerometer_interrupt_handler(void) {
+    *GPIO_ICR(gpio_port_a) |= (1 << 2); // Clear interrupt on pin 2
+    (void)accel_read(ACCEL_INT1_SRC_A);
 }
