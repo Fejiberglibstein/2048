c86c0089b379c9e6f478a8217742a0b909774965
Austin Thein <amthein321@gmail.com>
2025-11-11 09:44:04 -0500

    Fix comments for matrix_draw_pixel

--- 2048/matrix_asm.s
+++ 2048/matrix_asm.s
@@ -1,25 +1,24 @@
 .text
 
-	.ref matrix_get_wbuf 
+    .ref matrix_get_wbuf 
 
 ;; Set a pixel in the matrix
 ;;
 ;; # Parameters
-;; r0: pointer to the matrix
-;; r1: 32 bit color to set. The bit layout for the color format is as follows:
+;; r0: 32 bit color to set. The bit layout for the color format is as follows:
 ;;     (0000  0 b8 g8 r8  0000  0 b4 g4 r4  0000  0 b2 g2 r2  0000  0 b1 g1 r1)
-;; r2: x coordinate
-;; r3: y coordinate
+;; r1: x coordinate
+;; r2: y coordinate
     .def matrix_draw_pixel
 matrix_draw_pixel:
-	push {r4, lr, r11}
-	push {r0-r2}
-	bl matrix_get_wbuf
-	pop {r1-r3} ; This is reallllllly dumb
+    push {r4, lr, r11}
+    push {r0-r2}
+    bl matrix_get_wbuf
+    pop {r1-r3} ; Swap r0->r1, r1->r2, r2->r3
 
     cmp r3, #32
     it ge
-    subge r3, r3, #32     ; offset column by matrix height / 2
+    subge r3, r3, #32 ; offset column by matrix height / 2
 
     add r2, r2, r3, lsl #(6 + 2) ; r2 = (x + (y * 64 * 4))
 
@@ -33,6 +32,6 @@ matrix_draw_pixel:
         lsr r1, r1, #8       ; Shift to get the next color
     .endloop
 
-	pop {r4, lr, r11}
-	bx lr
+    pop {r4, lr, r11}
+    bx lr
 .end
