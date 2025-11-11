c85b53fa82f6e343f09a2b77decd834bb6f695dd
Austin Thein <amthein321@gmail.com>
2025-11-11 09:43:56 -0500

    Add header guards

--- 2048/include/matrix.h
+++ 2048/include/matrix.h
@@ -1,8 +1,13 @@
 #include "stdint.h"
 
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
 typedef uint32_t MatrixColor;
 
 void matrix_init();
 MatrixColor matrix_color(uint32_t r, uint32_t g, uint32_t b);
 void matrix_draw_pixel(MatrixColor color, uint8_t x, uint8_t y);
 void matrix_swap_bufs();
+
+#endif // _MATRIX_H
