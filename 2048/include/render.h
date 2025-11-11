15ff6fd52f2a7745b2d71aaaef10a4315078fcb6
Austin Thein <amthein321@gmail.com>
2025-11-11 09:43:56 -0500

    Add header guards

--- 2048/include/render.h
+++ 2048/include/render.h
@@ -1,3 +1,10 @@
 #include "stdint.h"
+#include "gameplay.h"
+
+#ifndef _RENDER_H
+#define _RENDER_H
 
 void render_tile(uint8_t x, uint8_t y, uint8_t num);
+void render_board(GameState *gs);
+
+#endif // _RENDER_H
