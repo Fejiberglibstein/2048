eed1f2c6e1e6b1571edd3b24c8ec48f371cf2ef1
Austin Thein <amthein321@gmail.com>
2025-11-18 17:03:00 -0500

    Get moving up to work correctly

--- 2048/gameplay.c
+++ 2048/gameplay.c
@@ -1,69 +1,11 @@
 #include "include/gameplay.h"
 #include "include/matrix.h"
 #include "include/render.h"
+#include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
 
-void game_clear(GameState *gs) {
-    int i, j;
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            gs->board[i][j] = 0;
-        }
-    }
-}
-
 struct Coord status[16];
-
-void game_new_random_tile(GameState *gs) {
-    int y, x;
-    uint32_t status_len = 0;
-    for (y = 0; y < 4; y++) {
-        for (x = 0; x < 4; x++) {
-            if (gs->board[y][x] == 0) {
-                // set i and j in the status
-                status[status_len++] = (struct Coord) {
-                    .y = y,
-                    .x = x,
-                };
-            }
-        }
-    }
-
-    uint8_t index = rand() % status_len;
-
-    struct Coord new_tile = status[index];
-    gs->board[new_tile.y][new_tile.x] = ((rand() % 100 < 70) ? 1 : 2);
-}
-
-void game_init(GameState *gs) {
-    srand(time(NULL));
-
-    // Ensure game state is clear
-    game_clear(gs);
-
-    // Generate 4 random coordinates
-    int r1 = rand() % 4;
-    int c1 = rand() % 4;
-
-    int r2, c2;
-    do { //  While coordinates match, try generating new ones
-        // Generate 4 random coordinates
-        r2 = rand() % 4;
-        c2 = rand() % 4;
-    } while ((r1 == r2) && (c1 == c2));
-
-    // Assign values to the blocks at these coordinates
-    gs->board[r1][c1] = 1; // First block guaranteed to be 2
-    int proc = rand() % 100;
-    uint8_t value_2;
-    if (proc < 80) {
-        value_2 = 1; // 80% chance of 2 for second block
-    } else {
-        value_2 = 2; // 20% chance of 4 for second block
-    }
-    gs->board[r2][c2] = value_2;
-}
 
 void shift_row(
     GameState *gs,
@@ -120,82 +62,63 @@ void shift_col(
 }
 
 void game_move_up(GameState *gs) {
-    int i = 0;
-    uint16_t smush_flag = 0;
-    for (i = 0; i < 3; i++) {
-        switch (i) {
-        case 2:
-            shift_row(gs, 3, 2, &smush_flag);
-        case 1:
-            shift_row(gs, 2, 1, &smush_flag);
-        case 0:
-            shift_row(gs, 1, 0, &smush_flag);
+    int i, j, start;
+    for (i = 0; i < 4; i++) { // Iterate over all the columns
+        // First four bits are for each tile in the col, if it is a 1 then the
+        // tile has already had a combination.
+        uint8_t combined_flag = 0;
+
+        for (start = 0; start < 3; start++) { // Iterate over every row
+            // Non-zero if the current tile being moved has been combined with
+            // another tile.
+            //
+            // Will have the value of `1 << n` where n is in the range (0, 3).
+            uint8_t tile_combined_at_position = 0;
+            for (j = start; j >= 0; j--) { // Iterate for every row
+                uint8_t *onto_tile = &gs->board[j][i];
+                uint8_t *from_tile = &gs->board[j + 1][i];
+
+                if (*onto_tile == 0) {
+                    *onto_tile = *from_tile;
+                    *from_tile = 0;
+                    continue;
+                }
+
+                if (combined_flag & (1 << j) || tile_combined_at_position) {
+                    // If we cannot move the tile up, but there has already been
+                    // a combination, then we shouldn't do anything else in the
+                    // loop
+                    break;
+                }
+
+                if (*onto_tile == *from_tile) {
+                    *onto_tile += 1;
+                    *from_tile = 0;
+                    // Set the onto tile's bitflag to be 1
+                    tile_combined_at_position = 1 << j;
+                    continue;
+                }
+            }
+            combined_flag |= tile_combined_at_position;
         }
     }
 }
 
-void game_move_down(GameState *gs) {
-    int i = 0;
-    uint16_t smush_flag = 0;
-    for (i = 0; i < 3; i++) {
-        switch (i) {
-        case 2:
-            shift_row(gs, 0, 1, &smush_flag);
-        case 1:
-            shift_row(gs, 1, 2, &smush_flag);
-        case 0:
-            shift_row(gs, 2, 3, &smush_flag);
-        }
-    }
-}
-
-void game_move_right(GameState *gs) {
-    int i = 0;
-    uint16_t smush_flag = 0;
-    for (i = 0; i < 3; i++) {
-        switch (i) {
-        case 2:
-            shift_col(gs, 3, 2, &smush_flag);
-        case 1:
-            shift_col(gs, 2, 1, &smush_flag);
-        case 0:
-            shift_col(gs, 1, 0, &smush_flag);
-        }
-    }
-}
-
-void game_move_left(GameState *gs) {
-    int i = 0;
-    uint16_t smush_flag = 0;
-    for (i = 0; i < 3; i++) {
-        switch (i) {
-        case 2:
-            shift_col(gs, 0, 1, &smush_flag);
-        case 1:
-            shift_col(gs, 1, 2, &smush_flag);
-        case 0:
-            shift_col(gs, 2, 3, &smush_flag);
-        }
-    }
-}
-
-void game_move_dir(GameState *gs, MoveDirection dir) {
-    switch (dir) {
-    case MOVE_UP:
-        game_move_up(gs);
-        break;
-    case MOVE_DOWN:
-        game_move_down(gs);
-        break;
-    case MOVE_LEFT:
-        game_move_left(gs);
-        break;
-    case MOVE_RIGHT:
-        game_move_right(gs);
-        break;
-    }
-
-    game_new_random_tile(gs);
-
-    render_board(gs);
-}
+// void game_move_dir(GameState *gs, MoveDirection dir) {
+//     switch (dir) {
+//     case MOVE_UP:
+//         game_move_up(gs);
+//         break;
+//     case MOVE_DOWN:
+//         game_move_down(gs);
+//         break;
+//     case MOVE_LEFT:
+//         game_move_left(gs);
+//         break;
+//     case MOVE_RIGHT:
+//         game_move_right(gs);
+//         break;
+//     }
+//
+//     render_board(gs);
+// }
