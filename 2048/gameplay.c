c4dd4fce0dfd834b6d065403fe3a8ac45a0f2c10
Austin Thein <amthein321@gmail.com>
2025-11-18 21:19:50 -0500

    Add spawning new tiles

--- 2048/gameplay.c
+++ 2048/gameplay.c
@@ -3,63 +3,8 @@
 #include "include/matrix.h"
 #include "include/render.h"
 #include <stdbool.h>
-#include <stdio.h>
-
-struct Coord status[16];
-
-void shift_row(
-    GameState *gs,
-    uint8_t from,
-    uint8_t onto,
-    uint16_t *smush_flag
-) {
-    int i;
-    for (i = 0; i < 4; i++) {
-        uint8_t *onto_tile = &gs->board[onto][i];
-        uint8_t *from_tile = &gs->board[from][i];
-
-        if (*smush_flag & 1 << ((i * 4) + onto)) {
-            continue;
-        }
-
-        if (*onto_tile == 0) {
-            *onto_tile = *from_tile;
-            *from_tile = 0;
-            // *smush_flag |= 1 << ((i * 4) + from);
-        } else if (*onto_tile == *from_tile) {
-            *onto_tile += 1;
-            *from_tile = 0;
-            *smush_flag |= 1 << ((i * 4) + onto);
-        }
-    }
-}
-
-void shift_col(
-    GameState *gs,
-    uint8_t from,
-    uint8_t onto,
-    uint16_t *smush_flag
-) {
-    int i;
-    for (i = 0; i < 4; i++) {
-        uint8_t *onto_tile = &gs->board[i][onto];
-        uint8_t *from_tile = &gs->board[i][from];
-
-        if (*smush_flag & 1 << ((onto * 4) + i)) {
-            continue;
-        }
-
-        if (*onto_tile == 0) {
-            *onto_tile = *from_tile;
-            *from_tile = 0;
-            // *smush_flag |= 1 << ((onto * 4) + i);
-        } else if (*onto_tile == *from_tile) {
-            *onto_tile += 1;
-            *from_tile = 0;
-            *smush_flag |= 1 << ((onto * 4) + i);
-        }
-    }
-}
+#include <stdlib.h>
+#include <string.h>
 
 void game_move_up(GameState *gs) {
     int i, j, start;
@@ -179,22 +124,21 @@ void game_move_down(GameState *gs) {
     }
 }
 
-
 void game_move_left(GameState *gs) {
     int i, j, start;
-    for (i = 0; i < 4; i++) { // Iterate over all the columns
-        // First four bits are for each tile in the col, if it is a 1 then the
+    for (i = 0; i < 4; i++) { // Iterate over all the rows
+        // First four bits are for each tile in the row, if it is a 1 then the
         // tile has already had a combination.
         uint8_t combined_flag = 0;
         bool tile_has_moved = false;
 
-        for (start = 0; start < 3; start++) { // Iterate over every row
+        for (start = 0; start < 3; start++) { // Iterate over every col
             // Non-zero if the current tile being moved has been combined with
             // another tile.
             //
             // Will have the value of `1 << n` where n is in the range (0, 3).
             uint8_t tile_combined_at_position = 0;
-            for (j = start; j >= 0; j--) { // Iterate for every row
+            for (j = start; j >= 0; j--) { // Iterate for every col
                 uint8_t *onto_tile = &gs->board[i][j];
                 uint8_t *from_tile = &gs->board[i][j + 1];
 
@@ -241,19 +185,19 @@ void game_move_left(GameState *gs) {
 
 void game_move_right(GameState *gs) {
     int i, j, start;
-    for (i = 0; i < 4; i++) { // Iterate over all the columns
-        // First four bits are for each tile in the col, if it is a 1 then the
+    for (i = 0; i < 4; i++) { // Iterate over all the rows
+        // First four bits are for each tile in the row, if it is a 1 then the
         // tile has already had a combination.
         uint8_t combined_flag = 0;
         bool tile_has_moved = false;
 
-        for (start = 3; start > 0; start--) { // Iterate over every row
+        for (start = 3; start > 0; start--) { // Iterate over every col
             // Non-zero if the current tile being moved has been combined with
             // another tile.
             //
             // Will have the value of `1 << n` where n is in the range (0, 3).
             uint8_t tile_combined_at_position = 0;
-            for (j = start; j < 4; j++) { // Iterate for every row
+            for (j = start; j < 4; j++) { // Iterate for every col
                 uint8_t *onto_tile = &gs->board[i][j];
                 uint8_t *from_tile = &gs->board[i][j - 1];
 
@@ -298,7 +242,34 @@ void game_move_right(GameState *gs) {
     }
 }
 
+struct Coord available_spawn_locations[16];
+void game_spawn_new_tile(GameState *gs) {
+    int x, y;
+    int avail_len = 0;
+
+    for (y = 0; y < 4; y++) {
+        for (x = 0; x < 4; x++) {
+            if (gs->board[y][x] == 0) {
+                available_spawn_locations[avail_len++] = (Coord) {
+                    .x = x,
+                    .y = y,
+                };
+            }
+        }
+    }
+
+    Coord spawn_loc = available_spawn_locations[rand() % avail_len];
+    // 75% chance to get a 1
+    uint8_t tile_num = ((rand() % 0xFF) < 0xBF) ? 1 : 2;
+    gs->board[spawn_loc.y][spawn_loc.x] = tile_num;
+
+    animation_new_spawn_anim(&gs->as, x, y);
+}
+
 void game_move_dir(GameState *gs, MoveDirection dir) {
+    uint8_t dup[4][4];
+    memcpy(&dup, &gs->board, 16);
+
     switch (dir) {
     case MOVE_UP:
         game_move_up(gs);
@@ -312,6 +283,11 @@ void game_move_dir(GameState *gs, MoveDirection dir) {
     case MOVE_RIGHT:
         game_move_right(gs);
         break;
+    }
+
+    // If the gameboard didn't change, then we can spawn a new tile
+    if (memcmp(dup, gs->board, 16) != 0) {
+        game_spawn_new_tile(gs);
     }
 
     render_board(gs);
