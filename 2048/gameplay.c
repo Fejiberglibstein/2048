c47b1c5d37d650d51064d424334841b4ad407baa
Austin Thein <amthein321@gmail.com>
2025-11-22 12:35:49 -0500

    Make the movement of tiles cleaner

--- 2048/gameplay.c
+++ 2048/gameplay.c
@@ -5,246 +5,125 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void game_move_up(GameState *gs) {
-    int i, j, start;
-    for (i = 0; i < 4; i++) { // Iterate over all the columns
-        // First four bits are for each tile in the col, if it is a 1 then the
-        // tile has already had a combination.
-        uint8_t combined_flag = 0;
+void game_spawn_new_tile(GameState *gs);
+
+void game_reset(GameState *gs) {
+    int x, y;
+    for (y = 0; y < 4; y++) {
+        for (x = 0; x < 4; x++) {
+            gs->board[y][x] = 0;
+        }
+    }
+}
+
+void game_init(GameState *gs) {
+    srand(time(NULL));
+
+    game_spawn_new_tile(gs);
+    gs->as = (AnimationState) {0};
+}
+
+void game_move_tiles(GameState *gs, Coord tiles[4]) {
+    int start, i;
+
+    // First four bits are for each tile in tiles, if the bit is a 1 then the
+    // tile has already had a combination.
+    uint8_t combined_flag = 0;
+    for (start = 0; start < 3; start++) {
         bool tile_has_moved = false;
 
-        for (start = 0; start < 3; start++) { // Iterate over every row
-            // Non-zero if the current tile being moved has been combined with
-            // another tile.
-            //
-            // Will have the value of `1 << n` where n is in the range (0, 3).
-            uint8_t tile_combined_at_position = 0;
-            uint8_t original_num = gs->board[start][i];
-            for (j = start; j >= 0; j--) { // Iterate for every row
-                uint8_t *onto_tile = &gs->board[j][i];
-                uint8_t *from_tile = &gs->board[j + 1][i];
+        const Coord tile_start = tiles[start + 1];
+        Coord tile_end;
+        // Non-zero if the current tile being moved has been combined with
+        // another tile.
+        //
+        // Will have the value of `1 << n` where n is in the range (0, 3).
+        uint8_t tile_combined_at_position = 0;
 
-                if (*from_tile == 0) {
-                    continue;
-                }
+        for (i = start; i >= 0; i--) {
+            const Coord onto_coord = tiles[i];
+            const Coord from_coord = tiles[i + 1];
+            uint8_t *onto_tile = &gs->board[onto_coord.y][onto_coord.x];
+            uint8_t *from_tile = &gs->board[from_coord.y][from_coord.x];
 
-                if (*onto_tile == 0) {
-                    *onto_tile = *from_tile;
-                    *from_tile = 0;
-                    tile_has_moved = true;
-                    continue;
-                }
+            if (*from_tile == 0) {
+                continue;
+            }
 
-                if (combined_flag & (1 << j)) {
-                    // If we cannot move the tile up, but there has already been
-                    // a combination, then we shouldn't do anything else in the
-                    // loop
-                    break;
-                }
+            if (*onto_tile == 0) {
+                *onto_tile = *from_tile;
+                *from_tile = 0;
+                tile_has_moved = true;
+                tile_end = onto_coord;
+                continue;
+            }
 
-                if (*onto_tile == *from_tile) {
-                    *onto_tile += 1;
-                    *from_tile = 0;
-                    // Set the onto tile's bitflag to be 1
-                    tile_combined_at_position = 1 << j;
-                    tile_has_moved = true;
-                    j -= 1; // Fix animation
-                    break;
-                }
-                // If the tile wasn't moved at all now, then it won't be moved
-                // in the future.
+            if (combined_flag & (1 << i)) {
+                // If we cannot move the tile up, but there has already been
+                // a combination, then we shouldn't do anything else in the
+                // loop
                 break;
             }
-            j++; // Fix animation
-            combined_flag |= tile_combined_at_position;
-            if (tile_has_moved) {
-                tile_has_moved = false;
-                animation_new_moved_anim(&gs->as, i, start + 1, i, j, original_num, gs->board[j][i]);
+
+            if (*onto_tile == *from_tile) {
+                *onto_tile += 1;
+                *from_tile = 0;
+                // Set the onto tile's bitflag to be 1
+                tile_combined_at_position = 1 << i;
+                tile_has_moved = true;
+                tile_end = onto_coord;
+                break;
             }
+            // If the tile wasn't moved at all now, then it won't be moved
+            // in the future.
+            break;
         }
+        combined_flag |= tile_combined_at_position;
+        if (tile_has_moved) {
+            tile_has_moved = false;
+            animation_new_moved_anim(
+                &gs->as,
+                tile_start.y,
+                tile_start.x,
+                tile_end.y,
+                tile_end.x,
+                gs->board[tile_start.y][tile_start.x],
+                gs->board[tile_end.y][tile_end.x]
+            );
+        }
+    }
+}
+
+void game_move_up(GameState *gs) {
+    int i;
+    for (i = 0; i < 4; i++) {
+        game_move_tiles(gs, (Coord[]) {{i, 0}, {i, 1}, {i, 2}, {i, 3}});
     }
 }
 
 void game_move_down(GameState *gs) {
-    int i, j, start;
-    for (i = 0; i < 4; i++) { // Iterate over all the columns
-        // First four bits are for each tile in the col, if it is a 1 then the
-        // tile has already had a combination.
-        uint8_t combined_flag = 0;
-        bool tile_has_moved = false;
-
-        for (start = 3; start > 0; start--) { // Iterate over every row
-            // Non-zero if the current tile being moved has been combined with
-            // another tile.
-            //
-            // Will have the value of `1 << n` where n is in the range (0, 3).
-            uint8_t tile_combined_at_position = 0;
-            uint8_t original_num = gs->board[start][i];
-            for (j = start; j < 4; j++) { // Iterate for every row
-                uint8_t *onto_tile = &gs->board[j][i];
-                uint8_t *from_tile = &gs->board[j - 1][i];
-
-                if (*from_tile == 0) {
-                    continue;
-                }
-
-                if (*onto_tile == 0) {
-                    *onto_tile = *from_tile;
-                    *from_tile = 0;
-                    tile_has_moved = true;
-                    continue;
-                }
-
-                if (combined_flag & (1 << j)) {
-                    // If we cannot move the tile up, but there has already been
-                    // a combination, then we shouldn't do anything else in the
-                    // loop
-                    break;
-                }
-
-                if (*onto_tile == *from_tile) {
-                    *onto_tile += 1;
-                    *from_tile = 0;
-                    // Set the onto tile's bitflag to be 1
-                    tile_combined_at_position = 1 << j;
-                    tile_has_moved = true;
-                    j += 1; // Fix animation
-                    break;
-                }
-                // If the tile wasn't moved at all now, then it won't be moved
-                // in the future.
-                break;
-            }
-            j -= 1; // Fix animation
-            combined_flag |= tile_combined_at_position;
-            if (tile_has_moved) {
-                tile_has_moved = false;
-                animation_new_moved_anim(&gs->as, i, start - 1, i, j, original_num, gs->board[j][i]);
-            }
-        }
-    }
-}
-
-void game_move_left(GameState *gs) {
-    int i, j, start;
-    for (i = 0; i < 4; i++) { // Iterate over all the rows
-        // First four bits are for each tile in the row, if it is a 1 then the
-        // tile has already had a combination.
-        uint8_t combined_flag = 0;
-        bool tile_has_moved = false;
-
-        for (start = 0; start < 3; start++) { // Iterate over every col
-            // Non-zero if the current tile being moved has been combined with
-            // another tile.
-            //
-            // Will have the value of `1 << n` where n is in the range (0, 3).
-            uint8_t tile_combined_at_position = 0;
-            uint8_t original_num = gs->board[i][start];
-            for (j = start; j >= 0; j--) { // Iterate for every col
-                uint8_t *onto_tile = &gs->board[i][j];
-                uint8_t *from_tile = &gs->board[i][j + 1];
-
-                if (*from_tile == 0) {
-                    continue;
-                }
-
-                if (*onto_tile == 0) {
-                    *onto_tile = *from_tile;
-                    *from_tile = 0;
-                    tile_has_moved = true;
-                    continue;
-                }
-
-                if (combined_flag & (1 << j)) {
-                    // If we cannot move the tile up, but there has already been
-                    // a combination, then we shouldn't do anything else in the
-                    // loop
-                    break;
-                }
-
-                if (*onto_tile == *from_tile) {
-                    *onto_tile += 1;
-                    *from_tile = 0;
-                    // Set the onto tile's bitflag to be 1
-                    tile_combined_at_position = 1 << j;
-                    tile_has_moved = true;
-                    j -= 1; // Fix animation
-                    break;
-                }
-                // If the tile wasn't moved at all now, then it won't be moved
-                // in the future.
-                break;
-            }
-            j++; // Fix animation
-            combined_flag |= tile_combined_at_position;
-            if (tile_has_moved) {
-                tile_has_moved = false;
-                animation_new_moved_anim(&gs->as, start + 1, i, j, i, original_num, gs->board[i][j]);
-            }
-        }
+    int i;
+    for (i = 0; i < 4; i++) {
+        game_move_tiles(gs, (Coord[]) {{i, 3}, {i, 2}, {i, 1}, {i, 0}});
     }
 }
 
 void game_move_right(GameState *gs) {
-    int i, j, start;
-    for (i = 0; i < 4; i++) { // Iterate over all the rows
-        // First four bits are for each tile in the row, if it is a 1 then the
-        // tile has already had a combination.
-        uint8_t combined_flag = 0;
-        bool tile_has_moved = false;
-
-        for (start = 3; start > 0; start--) { // Iterate over every col
-            // Non-zero if the current tile being moved has been combined with
-            // another tile.
-            //
-            // Will have the value of `1 << n` where n is in the range (0, 3).
-            uint8_t tile_combined_at_position = 0;
-            uint8_t original_num = gs->board[i][start];
-            for (j = start; j < 4; j++) { // Iterate for every col
-                uint8_t *onto_tile = &gs->board[i][j];
-                uint8_t *from_tile = &gs->board[i][j - 1];
-
-                if (*from_tile == 0) {
-                    continue;
-                }
-
-                if (*onto_tile == 0) {
-                    *onto_tile = *from_tile;
-                    *from_tile = 0;
-                    tile_has_moved = true;
-                    continue;
-                }
-
-                if (combined_flag & (1 << j)) {
-                    // If we cannot move the tile up, but there has already been
-                    // a combination, then we shouldn't do anything else in the
-                    // loop
-                    break;
-                }
-
-                if (*onto_tile == *from_tile) {
-                    *onto_tile += 1;
-                    *from_tile = 0;
-                    // Set the onto tile's bitflag to be 1
-                    tile_combined_at_position = 1 << j;
-                    tile_has_moved = true;
-                    j += 1; // Fix animation
-                    break;
-                }
-                // If the tile wasn't moved at all now, then it won't be moved
-                // in the future.
-                break;
-            }
-            j -= 1; // Fix animation
-            combined_flag |= tile_combined_at_position;
-            if (tile_has_moved) {
-                tile_has_moved = false;
-                animation_new_moved_anim(&gs->as, start - 1, i, j, i, original_num, gs->board[i][j]);
-            }
-        }
+    int i;
+    for (i = 0; i < 4; i++) {
+        game_move_tiles(gs, (Coord[]) {{0, i}, {1, i}, {2, i}, {3, i}});
     }
 }
+
+void game_move_left(GameState *gs) {
+    int i;
+    for (i = 0; i < 4; i++) {
+        game_move_tiles(gs, (Coord[]) {{3, i}, {2, i}, {1, i}, {0, i}});
+    }
+}
+
 
 struct Coord available_spawn_locations[16];
 void game_spawn_new_tile(GameState *gs) {
@@ -267,12 +146,13 @@ void game_spawn_new_tile(GameState *gs) {
     uint8_t tile_num = ((rand() % 0xFF) < 0xBF) ? 1 : 2;
     gs->board[spawn_loc.y][spawn_loc.x] = tile_num;
 
-    animation_new_spawn_anim(&gs->as, x, y, tile_num);
+    animation_new_spawn_anim(&gs->as, spawn_loc.x, spawn_loc.y, tile_num);
 }
 
 void game_move_dir(GameState *gs, MoveDirection dir) {
     uint8_t dup[4][4];
     memcpy(&dup, &gs->board, 16);
+    gs->as.animations_length = 0;
 
     switch (dir) {
     case MOVE_UP:
@@ -308,5 +188,4 @@ void game_move_dir(GameState *gs, MoveDirection dir) {
 
     gs->as.current_direction = dir;
     gs->as.frame_number = 0;
-    render_board(gs->board);
 }
