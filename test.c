e8181c40418d7e288e35424dc3696d6d9a219994
Austin Thein <amthein321@gmail.com>
2025-11-22 20:19:52 -0500

    Add more proper testing for gameplay

--- test.c
+++ test.c
@@ -1,37 +1,76 @@
 #include "2048/include/gameplay.h"
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 
+GameState gs;
+
 void print_board(GameState *gs);
+bool board_is_eq(GameState g1, GameState g2);
 
 int main() {
     GameState gs = {
         .board = {
-            {1, 0, 2, 2},
-            {1, 1, 1, 1},
+            {0, 0, 2, 1},
             {2, 1, 1, 1},
-            {2, 2, 0, 1},
-        },
-
-        // .board = {
-        //     {0, 0, 0, 0},
-        //     {0, 0, 0, 0},
-        //     {0, 0, 0, 0},
-        //     {0, 0, 0, 1},
-        // },
-    };
-
-    GameState expected = {
-        .board = {
-            {5, 4, 3, 2},
-            {0, 0, 0, 2},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
+            {1, 1, 1, 2},
+            {1, 2, 0, 0},
         },
     };
-
     game_move_up(&gs);
+    assert(board_is_eq(
+        gs,
+        (GameState) {
+            .board = {
+                {2, 2, 2, 2},
+                {2, 2, 2, 2},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+            },
+        }
+    ));
 
-    print_board(&gs);
+    gs = (GameState) {
+        .board = {
+            {0, 2, 1, 1},
+            {0, 0, 1, 0},
+            {1, 1, 0, 0},
+            {0, 0, 0, 0},
+        },
+    };
+    game_move_left(&gs);
+    assert(board_is_eq(
+        gs,
+        (GameState) {
+            .board = {
+                {2, 2, 2, 2},
+                {2, 2, 2, 2},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+            },
+        }
+    ));
+
+    gs = (GameState) {
+        .board = {
+            {1, 1, 2, 2},
+            {0, 1, 1, 0},
+            {1, 0, 0, 1},
+            {2, 2, 1, 1},
+        },
+    };
+    game_move_up(&gs);
+    assert(board_is_eq(
+        gs,
+        (GameState) {
+            .board = {
+                {2, 2, 2, 2},
+                {2, 2, 2, 2},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+            },
+        }
+    ));
 }
 
 void print_board(GameState *gs) {
@@ -41,4 +80,16 @@ void print_board(GameState *gs) {
         }
         printf("\n");
     }
+}
+
+bool board_is_eq(GameState g1, GameState g2) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (g1.board[i][j] != g2.board[i][j]) {
+                print_board(&g1);
+                return false;
+            }
+        }
+    }
+    return true;
 }
